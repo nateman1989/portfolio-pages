aa35a188dbef14e008bbd0e1c72d58ac6e388545
nateman1989 <52223271+nateman1989@users.noreply.github.com>
2019-08-16 04:10:27 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    // Get the background color of the first list item.
+    var backgroundColor = $('.color').css('background-color');
+
+
+    // Set several properties on all list items.
+    $('.color').css({
+        'background-color': 'white',
+
+    });
+});
